5be9ee005f79746bd7f6544db6055a8d0e87f574
LeiDeng <37464179+summerdenglei@users.noreply.github.com>
2023-04-21 18:38:42 +0800

    Update DefineParam.py

--- DefineParam.py
+++ DefineParam.py
@@ -18,7 +18,7 @@ def get_param():
 
     trainFile = './trainData/graph_traffic_train.mat' #training data
     #trainFile = 'E:/DengLei/Transform-Based-Tensor-Net-master\trainData/trainUCF.mat'
-    valFile = './valData/graph_traffic_val.mat'  #test data
+    valFile = './valData/graph_traffic_val.mat'  #test dataset. Validation dataset is also the test dataset.
 
     saveDir = './recImg/recImgFinal'
     modelDir = './Model_Int'
